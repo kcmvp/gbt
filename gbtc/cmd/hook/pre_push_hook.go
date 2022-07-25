1e70a67d845d35d8783c37f6ab38167cfb5a627a
kcmvp <kcheng.mvp@gmail.com>
2022-07-25 11:21:11 +0800

    #14: change to 0.7

--- gbtc/cmd/hook/pre_push_hook.go
+++ gbtc/cmd/hook/pre_push_hook.go
@@ -4,18 +4,24 @@ package main
 
 import (
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/kcmvp/gbt/script"
 	"log"
 	"os"
 )
 
 func main() {
-	git.PlainOpen("")
 	cqc := script.NewCQC(0.35, 0.85)
 	cqc.Clean().Test()
 	if cqc.Error() != nil {
 		log.Fatalf("%v", cqc.Error())
 	} else {
+		// changed but not committed will abort the push
+
+		rep, _ := git.PlainOpen("")
+		rep.Head()
+		commit, _ := object.GetCommit()
+		commit.Patch()
 		// scripts/coverage.data must be exists
 		// todo compare coverage with the test output must be equal
 		// todo local coverage must >= remote coverage
